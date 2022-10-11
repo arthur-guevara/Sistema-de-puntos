082ee3a6aa44d0f0d62231316e00c0245f116270
arthur-guevara <86362708+arthur-guevara@users.noreply.github.com>
2022-10-11 18:29:15 -0500

    Commit inicial

---
+++ app/Views/elements/footer.php
@@ -0,0 +1,5 @@
+<footer class="text-muted py-5">
+    <div class="container">
+        <p class="mb-1">&copy; 2022 Made with ❤ by <a href="https://github.com/arthur-guevara">Arthur</a></p>
+    </div>
+</footer>
